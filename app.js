55a27271d5e95f18882c6532caab46ef3dbdb9ca
DennisJohn <dennis.djp@gmail.com>
2016-12-06 21:48:39 -0600

    react progress

---
+++ app.js
@@ -0,0 +1,12 @@
+class Channel extends React.Component{
+	onClick() {
+			console.log('I was clicked', this.props.name);
+	}
+	render(){
+		return (
+			<li onClick={this.onClick.bind(this)}>{this.props.name}</li>
+			)
+	}
+}
+
+ReactDOM.render(<Channel name='Test support'/>, document.getElementById('app'));
