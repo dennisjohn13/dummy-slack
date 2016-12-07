651b4dbea532efb82c9e1c53c2286b54f7e6e408
DennisJohn <dennis.djp@gmail.com>
2016-12-07 13:32:19 -0600

    React sudy

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 class Channel extends React.Component{
 	onClick() {
-			console.log('I was clicked', this.props.name);
+			console.log('I was clicked', this.props.namePassed);
 	}
 	render(){
 		return (
-			<li onClick={this.onClick.bind(this)}>{this.props.name}</li>
+			<li onClick={this.onClick.bind(this)}>{this.props.namePassed}</li>
 			)
 	}
 }
 
-ReactDOM.render(<Channel name='Test support'/>, document.getElementById('app'));
+ReactDOM.render(<Channel namePassed='Test support1'/>, document.getElementById('app'));
