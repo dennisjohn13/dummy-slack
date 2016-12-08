7527a88efe2afa26afc90afa049ae451bac764cc
DennisJohn <dennis.djp@gmail.com>
2016-12-07 23:45:02 -0600

    Working on React

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
+let channels = [
+	{name : 'Test Support'},
+	{name : 'Hardware Support'},
+	{name : 'Software Support'}
+]
+
 class Channel extends React.Component{
 	onClick() {
-			console.log('I was clicked', this.props.namePassed);
+			console.log('I was clicked', this.props.name);
 	}
 	render(){
 		return (
-			<li onClick={this.onClick.bind(this)}>{this.props.namePassed}</li>
-			)
+			<li onClick={this.onClick.bind(this)}>{this.props.name}</li>
+			   )
 	}
 }
 
-ReactDOM.render(<Channel namePassed='Test support1'/>, document.getElementById('app'));
+class ChannelList extends React.Component{
+		render() {
+			return(
+				<ul>
+					<Channel name='Hadrdware support' />
+				</ul>
+			)
+		}
+}
+
+ReactDOM.render(<ChannelList />, document.getElementById('app'));
